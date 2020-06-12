correct pre_transform(10m frames) + change data augmentation (rotation on z axis only)

torch_points3d/datasets/registration/base_kitti.py
@@ -43,6 +43,32 @@ def read_calib_file(filepath):
     return T_calib
 
 
+def compute_spaced_time_frame(list_name_frames, path, min_dist):
+    """
+    compute spaced time frame to have different pairs.
+    Inspired by
+    """
+    list_pair = []
+    list_sensor_pos = []
+    for name in list_name_frames:
+        data = torch.load(osp.join(path, name))
+        list_sensor_pos.append(data.pos_sensor)
+
+    list_sensor_pos = torch.stack(list_sensor_pos)
+
+    mask = ((list_sensor_pos.unsqueeze(0) - list_sensor_pos.unsqueeze(1)) ** 2).sum(2) > (min_dist ** 2)
+    curr_ind = 0
+
+    for curr_ind in range(len(list_name_frames)):
+        candidates = torch.where(mask[curr_ind, curr_ind:curr_ind+100])[0]
+        if(len(candidates) > 0):
+            new_ind = curr_ind + candidates[0].item()
+            list_pair.append((curr_ind, new_ind))
+        else:
+            continue
+    return list_pair
+
+
 class BaseKitti(Dataset):
     sequence_train = [0, 1, 2, 3, 4, 5]
     sequence_val = [6, 7]
@@ -53,6 +79,7 @@ class BaseKitti(Dataset):
                  mode='train',
                  max_dist_overlap=0.01,
                  max_time_distance=3,
+                 min_dist=10,
                  transform=None,
                  pre_transform=None,
                  pre_filter=None):
@@ -64,6 +91,7 @@ class BaseKitti(Dataset):
         self.mode = mode
         self.max_dist_overlap = max_dist_overlap
         self.max_time_distance = max_time_distance
+        self.min_dist = min_dist
         if mode not in self.dict_seq.keys():
             raise RuntimeError('this mode {} does '
                                'not exist'
@@ -112,9 +140,11 @@ class BaseKitti(Dataset):
                 xyzr = np.fromfile(osp.join(path_frames, name), dtype=np.float32).reshape((-1, 4))
                 xyzr[:, :3] = xyzr[:, :3].dot(T_calib[:3, :3].T) + T_calib[:3, 3]
                 xyzr[:, :3] = xyzr[:, :3].dot(pose[:3, :3].T) + pose[:3, 3]
-
+                # store position of the car to filter some frames
+                pos_sensor = pose[:3, :3].dot(T_calib[:3, 3]) + pose[:3, 3]
                 data = Data(pos=torch.from_numpy(xyzr[:, :3]),
-                            reflectance=torch.from_numpy(xyzr[:, 3]))
+                            reflectance=torch.from_numpy(xyzr[:, 3]),
+                            pos_sensor=torch.from_numpy(pos_sensor))
                 out_path = osp.join(out_dir, name.split('.')[0] + ".pt")
                 if(self.pre_transform is not None):
                     data = self.pre_transform(data)
@@ -132,21 +162,31 @@ class BaseKitti(Dataset):
             path_fragment = osp.join(self.processed_dir, mod, "fragment", "{:02d}".format(drive))
             list_name_frames = sorted([f for f in os.listdir(path_fragment) if "pt" in f])
 
-            for i in range(len(list_name_frames)):
-                for j in range(len(list_name_frames)):
-                    if((j - i) > 0 and (j - i) < self.max_time_distance):
-                        out_path = osp.join(out_dir,
-                                            'matches{:06d}.npy'.format(ind))
-                        path1 = osp.join(path_fragment, list_name_frames[i])
-                        path2 = osp.join(path_fragment, list_name_frames[j])
-                        data1 = torch.load(path1)
-                        data2 = torch.load(path2)
-                        match = compute_overlap_and_matches(
-                            data1, data2, self.max_dist_overlap)
-                        match['path_source'] = path1
-                        match['path_target'] = path2
-                        np.save(out_path, match)
-                        ind += 1
+            # pre_compute specific pair
+            log.info("Compute the pairs")
+            time_frame = []
+            if(self.min_dist is not None):
+                pair_time_frame = compute_spaced_time_frame(
+                    list_name_frames, path_fragment, self.min_dist)
+            else:
+                pair_time_frame = [(i, j)
+                                   for i in range(len(list_name_frames))
+                                   for j in range(len(list_name_frames))
+                                   if (j - i) > 0 and (j - i) < self.max_time_distance]
+            log.info("Compute the matches")
+            for i, j in pair_time_frame:
+                out_path = osp.join(out_dir,
+                                    'matches{:06d}.npy'.format(ind))
+                path1 = osp.join(path_fragment, list_name_frames[i])
+                path2 = osp.join(path_fragment, list_name_frames[j])
+                data1 = torch.load(path1)
+                data2 = torch.load(path2)
+                match = compute_overlap_and_matches(
+                    data1, data2, self.max_dist_overlap)
+                match['path_source'] = path1
+                match['path_target'] = path2
+                np.save(out_path, match)
+                ind += 1
 
     def process(self):
         log.info("pre_transform those fragments")