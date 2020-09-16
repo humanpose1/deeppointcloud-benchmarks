14f695f0c50a208c61a1182036b146de14b043d9
humanpose1 <sofiane.horache@mines-paristech.fr>
2020-09-16 10:29:13 +0200

    better results with this implementation

--- torch_points3d/models/registration/emhs.py
+++ torch_points3d/models/registration/emhs.py
@@ -1,6 +1,7 @@
 import torch
 
-from torch_points3d.modules.MinkowskiEngine.equivariant_map import SuperEMHS
+from torch_points3d.modules.MinkowskiEngine.equivariant_map import *
+from torch_points3d.core.base_conv.partial_dense import *
 from torch_points3d.models.base_architectures import UnwrappedUnetBasedModel
 from torch_points3d.models.registration.base import FragmentBaseModel
 from torch_points3d.models.registration.minkowski import BaseMinkowski
@@ -8,18 +9,10 @@ from torch_points3d.core.common_modules import FastBatchNorm1d, Seq
 from torch_geometric.data import Data
 
 
-class EMHS_Model(FragmentBaseModel):
+class EMHS_Model(FragmentBaseModel, UnwrappedUnetBasedModel):
     def __init__(self, option, model_type, dataset, modules):
-        FragmentBaseModel.__init__(self, option)
+        UnwrappedUnetBasedModel.__init__(self, option, model_type, dataset, modules)
 
-        self.model = SuperEMHS(
-            D=3,
-            dilation=1,
-            dim_feat=option.dim_feat,
-            num_layer=option.num_layer,
-            grid_size=option.grid_size,
-            L=option.L,
-        )
         self.normalize_feature = option.normalize_feature
         self.mode = option.loss_mode
         self.loss_names = ["loss_reg", "loss"]
@@ -57,8 +50,27 @@ class EMHS_Model(FragmentBaseModel):
             return input
 
     def apply_nn(self, input):
-        output = self.model(input).x
+        stack_down = []
+        data = input
+        for i in range(len(self.down_modules) - 1):
+            data = self.down_modules[i](data)
+            stack_down.append(data)
+
+        data = self.down_modules[-1](data)
+        innermost = False
+
+        if not isinstance(self.inner_modules[0], torch.nn.Identity):
+            stack_down.append(data)
+            data = self.inner_modules[0](data)
+            innermost = True
+
+        for i in range(len(self.up_modules)):
+            if i == 0 and innermost:
+                data = self.up_modules[i]((data, stack_down.pop()))
+            else:
+                data = self.up_modules[i]((data, stack_down.pop()))
+        output = data.x
         if self.normalize_feature:
-            return output / (torch.norm(output, p=2, dim=1, keepdim=True) + 1e-3)
+            return output / (torch.norm(output, p=2, dim=1, keepdim=True) + 1e-20)
         else:
             return output
--- torch_points3d/modules/MinkowskiEngine/equivariant_map.py
+++ torch_points3d/modules/MinkowskiEngine/equivariant_map.py
@@ -3,151 +3,117 @@ import torch.nn as nn
 import MinkowskiEngine as ME
 from .common import ConvType, NormType
 
+from .modules import ResnetBlockDown
+
+from torch_points3d.core.common_modules.base_modules import BaseModule
+
 from torch_points3d.utils.config import is_list
 from torch_points3d.core.common_modules.base_modules import FastBatchNorm1d, MLP
-
-from torch_geometric.data import Batch
+from torch_points3d.core.data_transform import GridSampling3D
 from torch_geometric.nn.pool.consecutive import consecutive_cluster
 from torch_geometric.nn import voxel_grid
 from torch_scatter import scatter_mean
 
 
-class OuterEquivariantMap(nn.Module):
-    def __init__(self, input_nc=3, output_nc=64, dilation=1, size_conv=3, D=3):
-
-        super(OuterEquivariantMap, self).__init__()
-
-        self.conv3d = ME.MinkowskiConvolution(
-            input_nc, output_nc, kernel_size=size_conv, stride=1, dilation=dilation, dimension=D
-        )
-
-        self.conv = torch.nn.Linear(input_nc + output_nc, output_nc)
-
-    def _prepare_and_pool(self, data):
-        assert hasattr(data, "coords")
-        assert hasattr(data, "cluster")
-        assert hasattr(data, "unique_pos_indices")
-        x = scatter_mean(data.x, data.cluster, dim=0)
-        batch = data.batch[data.unique_pos_indices]
-        coords = torch.cat([batch.unsqueeze(-1).float(), data.coords.float()], -1)
-        sparse_tensor = ME.SparseTensor(x, coords=coords)
-        return sparse_tensor
-
-    def forward(self, data):
-
-        st = self._prepare_and_pool(data)
-        out = self.conv3d(st)
-        u = self.conv(torch.cat([data.x, out.F[data.cluster]], -1))
-        data.x = u
-        return data
-
-
-class InnerEquivariantMap(nn.Module):
-    """
-    TODO: add attention mechanism
-    """
-
-    def __init__(self, input_nc, output_nc, L=1, D=3):
-        super(InnerEquivariantMap, self).__init__()
-        channels = [input_nc] + [output_nc for _ in range(L)]
-        # self.conv1 = nn.Linear(input_nc, output_nc)
-        self.conv1 = MLP(channels)
-
-    def forward(self, data):
-
-        data.x = self.conv1(data.x)
-        return data
-
-
-class EMLayer(nn.Module):
-    def __init__(
-        self, input_nc, output_nc, L=2, size_conv=3, dilation=1, bn_momentum=0.02, D=3,
-    ):
-        super(EMLayer, self).__init__()
-        self.oem = OuterEquivariantMap(input_nc, output_nc, dilation, size_conv, D)
-        self.iem = InnerEquivariantMap(input_nc, output_nc, L, D)
-        self.norm = FastBatchNorm1d(output_nc, momentum=bn_momentum)
-        self.activation = nn.ReLU()
-
-    def forward(self, data):
-        inner = Batch(x=data.x, cluster=data.cluster)
-        data.x = self.activation(self.norm(self.oem(data).x + self.iem(inner).x))
-        return data
-
-
-class ResEMBlock(nn.Module):
-    def __init__(
-        self, input_nc, output_nc, L=2, dilation=1, size_conv=3, bn_momentum=0.02, D=3,
-    ):
-        super(ResEMBlock, self).__init__()
-        self.em1 = EMLayer(
-            input_nc, output_nc, L=L, dilation=dilation, size_conv=size_conv, bn_momentum=bn_momentum, D=D
-        )
-        self.em2 = EMLayer(
-            output_nc, output_nc, L=L, dilation=dilation, size_conv=size_conv, bn_momentum=bn_momentum, D=D
-        )
-
-    def forward(self, data):
-        residual = data.x
-        out = self.em2(self.em1(data))
-        out.x += residual
-        return out
-
-
-class EquivariantMapNetwork(nn.Module):
-    def __init__(self, input_nc=3, dim_feat=64, output_nc=32, grid_size=0.1, L=2, num_layer=20, dilation=1, D=3):
-        super(EquivariantMapNetwork, self).__init__()
-        self.layer1 = EMLayer(input_nc, dim_feat, dilation=dilation, D=D, L=1)
-        self.list_res = nn.ModuleList()
-        for _ in range(num_layer):
-            self.list_res.append(ResEMBlock(dim_feat, dim_feat, dilation=dilation, D=D, L=L))
-        self._grid_size = grid_size
+class CoarseVoxelBlock(nn.Module):
+    def __init__(self, channels, voxel_size=0.1, kernel_size=3, dilation=1, bn_momentum=0.05, dimension=3):
+        super(CoarseVoxelBlock, self).__init__()
+        self._grid_size = voxel_size
+        modules = []
+        for i in range(1, len(channels)):
+            block = ResnetBlockDown(
+                down_conv_nn=[channels[i - 1], channels[i], channels[i]],
+                kernel_size=kernel_size,
+                stride=1,
+                dimension=dimension,
+                dilation=dilation,
+            )
+            modules.append(block)
+        self.mod = nn.Sequential(*modules)
 
     def _prepare_data(self, data):
         coords = torch.round((data.pos) / self._grid_size)
         cluster = voxel_grid(coords, data.batch, 1)
         cluster, unique_pos_indices = consecutive_cluster(cluster)
-        data.cluster = cluster
-        data.coords = coords[unique_pos_indices].int()
-        data.unique_pos_indices = unique_pos_indices
-        return data
+
+        coords = coords[unique_pos_indices]
+        coords = torch.cat([data.batch[unique_pos_indices].unsqueeze(-1).float(), coords.float()], -1)
+        x = scatter_mean(data.x, cluster, dim=0)
+        sparse_tensor = ME.SparseTensor(x, coords=coords)
+        return sparse_tensor, cluster
 
     def forward(self, data):
-        x = self._prepare_data(data)
-        x = self.layer1(x)
-        for i in range(len(self.list_res)):
-            x = self.list_res[i](x)
-        return x
+        d = data.clone()
+        sparse_tensor, cluster = self._prepare_data(d)
+        sparse_tensor = self.mod(sparse_tensor)
+        d.x = sparse_tensor.F[cluster]
+        return d
 
 
-class SuperEMHS(nn.Module):
+class FineBlock(nn.Module):
+    def __init__(self, channels, bn_momentum=0.02, activation=nn.LeakyReLU(0.1)):
+        super(FineBlock, self).__init__()
+        self.mlp = MLP(channels, bn_momentum=bn_momentum, activation=activation)
+
+    def forward(self, data, precomputed=None, **kwargs):
+        d = data.clone()
+        d.x = self.mlp(d.x)
+        return d
+
+
+class EMHSBlock(BaseModule):
+    """
+    Perform a EMHSBlock which is two EMHS layers and
+    """
+
     def __init__(
         self,
-        dim_feat=[3, 64, 64, 64, 32],
-        num_layer=[4, 4, 4, 1],
-        grid_size=[0.1, 0.2, 0.4, 0.8],
+        down_conv_nn=None,
+        grid_size=None,
+        prev_grid_size=None,
+        bn_momentum=0.02,
+        activation=torch.nn.LeakyReLU(negative_slope=0.1),
+        voxel_size=0.1,
+        kernel_size=3,
         dilation=1,
-        D=3,
-        L=[1, 2, 2, 2],
+        num_layer_mlp=2,
+        num_layer_conv=2,
+        **kwargs
     ):
-        super(SuperEMHS, self).__init__()
-        self.list_nn = nn.ModuleList()
-        for i in range(1, len(dim_feat)):
-            self.list_nn.append(
-                EquivariantMapNetwork(
-                    dim_feat[i - 1],
-                    dim_feat[i],
-                    dim_feat[i],
-                    grid_size=grid_size[i - 1],
-                    num_layer=num_layer[i - 1],
-                    L=L[i - 1],
-                    dilation=dilation,
-                    D=D,
-                )
-            )
+        super(EMHSBlock, self).__init__()
+        assert len(down_conv_nn) == 2
 
-    def forward(self, data):
-        x = data
-        for i in range(len(self.list_nn)):
-            x = self.list_nn[i](x)
-        return x
+        num_inputs, num_outputs = down_conv_nn
+        self.is_strided = prev_grid_size != grid_size
+        channels_conv = [down_conv_nn[0]] + [down_conv_nn[1] for _ in range(num_layer_conv)]
+        channels_mlp = [down_conv_nn[0]] + [down_conv_nn[1] for _ in range(num_layer_mlp)]
+        if num_layer_conv >= 1:
+            self.voxel = CoarseVoxelBlock(
+                channels=channels_conv,
+                bn_momentum=bn_momentum,
+                voxel_size=voxel_size,
+                kernel_size=kernel_size,
+                dilation=dilation,
+            )
+        else:
+            self.voxel = None
+
+        self.mlp = FineBlock(channels=channels_mlp, bn_momentum=bn_momentum, activation=activation)
+        if self.is_strided:
+            self.pool = GridSampling3D(grid_size)
+
+    def forward(self, data, **kwargs):
+
+        out_v = data.clone()
+        out_mlp = data.clone()
+        out_mlp = self.mlp(out_mlp)
+        if self.voxel is not None:
+            out_v = self.voxel(out_v)
+        else:
+            out_v = out_mlp
+
+        assert out_v.x.shape == out_mlp.x.shape
+        out_mlp.x = out_mlp.x + out_v.x
+        if self.is_strided:
+            out_mlp = self.pool(out_mlp)
+        return out_mlp
