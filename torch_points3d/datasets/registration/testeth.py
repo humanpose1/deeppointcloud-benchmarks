723053ea54d4cffe56d395401de6aec64cd68562
humanpose1 <sofiane.horache@mines-paristech.fr>
2021-01-11 11:01:39 +0100

    add None

--- torch_points3d/datasets/registration/testeth.py
+++ torch_points3d/datasets/registration/testeth.py
@@ -158,6 +158,7 @@ class ETHDataset(BaseSiameseDataset):
                                          min_points=dataset_opt.min_points,
                                          ss_transform=ss_transform,
                                          use_fps=dataset_opt.use_fps)
+        self.val_dataset = None
         self.test_dataset = TestPairETH(root=self._data_path,
                                         pre_transform=pre_transform,
                                         transform=test_transform,
